2fc7dc418689008265a3d224d838f4173420eac1
LT <support@teran.tech>
2020-12-26 12:19:54 -0700

    Lab Project - Part 3    - Player Movement       - Constraining bounds       - Modified player mass and jump force    - Camera following player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
    private bool isOnGround = true;
    private float speed = 125.0f;
-   private float jumpForce = 500.0f;
+   private float jumpForce = 150.0f;
+   private float areaBounds = 20.0f;
+
    private Rigidbody playerRb;
 
    // Start is called before the first frame update
@@ -18,17 +20,9 @@ public class PlayerController : MonoBehaviour
    // Update is called once per frame
    void Update()
    {
-      float horizontalInput = Input.GetAxis("Horizontal");
-      float verticalInput = Input.GetAxis("Vertical");
-
-      playerRb.AddForce(Vector3.forward * speed * verticalInput);
-      playerRb.AddForce(Vector3.right * speed * horizontalInput);
-
-      if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
-      {
-         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-         isOnGround = false;
-      }
+      Player_Move();
+      Player_Jump();
+      Player_Bounds();
    }
 
    void OnCollisionEnter(Collision other)
@@ -36,6 +30,47 @@ public class PlayerController : MonoBehaviour
       if (other.gameObject.CompareTag("Ground"))
       {
          isOnGround = true;
+      }
+   }
+
+   void Player_Move()
+   {
+      float horizontalInput = Input.GetAxis("Horizontal");
+      float verticalInput = Input.GetAxis("Vertical");
+
+      playerRb.AddForce(Vector3.forward * speed * verticalInput);
+      playerRb.AddForce(Vector3.right * speed * horizontalInput);
+   }
+
+   void Player_Jump()
+   {
+      if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+      {
+         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+         isOnGround = false;
+      }
+   }
+
+   void Player_Bounds()
+   {
+      if (transform.position.x > areaBounds)
+      {
+         transform.position = new Vector3(areaBounds, transform.position.y, transform.position.z);
+      }
+
+      if (transform.position.x < -areaBounds)
+      {
+         transform.position = new Vector3(-areaBounds, transform.position.y, transform.position.z);
+      }
+
+      if (transform.position.z > areaBounds)
+      {
+         transform.position = new Vector3(transform.position.x, transform.position.y, areaBounds);
+      }
+
+      if (transform.position.z < -areaBounds)
+      {
+         transform.position = new Vector3(transform.position.x, transform.position.y, -areaBounds);
       }
    }
 }
